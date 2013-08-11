5b877c3eaaf217de90e15bf1d6a785203407847e
Alvis <alvisstar1991@gmail.com>
2013-08-11 18:33:21 +0700

    Them class

--- VietSearch.Configuration/GlobalConfig.cs
+++ VietSearch.Configuration/GlobalConfig.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Configuration;
 namespace VietSearch.Configuration
 {
     public class GlobalConfig
     {
+        public static String ConnectionString
+        {
+
+            get
+            {
+               
+                return string.Empty;
+            }
+        }
     }
 }
--- VietSearchWebService/Models/ModelObject/Appoint.cs
+++ VietSearchWebService/Models/ModelObject/Appoint.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace VietSearchWebService.Models.ModelObject
+{
+    public class Appoint
+    {
+    }
+}
