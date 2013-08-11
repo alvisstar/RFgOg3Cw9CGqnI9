1ef5bc0bc124d92e80aefab355872439e6bf67e1
Alvis <alvisstar1991@gmail.com>
2013-08-11 18:20:13 +0700

    Khoi tao du an

---
+++ VietSearch.DefineType/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VietSearch.DefineType
+{
+    public class Coordinate
+    {
+        float _Longitude;
+
+        public float Longitude
+        {
+            get { return _Longitude; }
+            set { _Longitude = value; }
+        }
+        float _Latitude;
+
+        public float Latitude
+        {
+            get { return _Latitude; }
+            set { _Latitude = value; }
+        }
+    }
+}
---
+++ VietSearchWebService/Models/ModelManager/StreetManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VietSearchWebService.Models.ModelManager
+{
+    public class StreetManager
+    {
+    }
+}
